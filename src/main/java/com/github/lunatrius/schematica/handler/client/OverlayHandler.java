9bef8dffccb0df3abd4735fd09ac633dccd6169c
alex-huff <alexhuff@bakahuff.com>
2024-03-30 22:12:26 -0500

    Use correct class for raytrace

--- src/main/java/com/github/lunatrius/schematica/handler/client/OverlayHandler.java
+++ src/main/java/com/github/lunatrius/schematica/handler/client/OverlayHandler.java
@@ -44,14 +44,14 @@ public class OverlayHandler {
 
                     final BlockPos offsetPos = pos.add(schematic.position);
                     String lookMessage = String.format("Looking at: %d %d %d (%d %d %d)", pos.getX(), pos.getY(), pos.getZ(), offsetPos.getX(), offsetPos.getY(), offsetPos.getZ());
-                    if (this.minecraft.objectMouseOver != null && this.minecraft.objectMouseOver.typeOfHit == RayTraceResult.Type.BLOCK) {
+                    if (this.minecraft.objectMouseOver != null && this.minecraft.objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
                         final BlockPos origPos = this.minecraft.objectMouseOver.getBlockPos();
                         if (offsetPos.equals(origPos)) {
                             lookMessage += " (matches)";
                         }
                     }
 
-                    left.add(SCHEMATICA_PREFIX + lookMessage);
+                    event.left.add("[§6Schematica§r] " + lookMessage);
                 }
             }
         }
